ffa531b9651dbd1a967278586413ee4c889e79e7
Karolina Nowak <nowakina15@gmail.com>
2024-04-05 17:46:42 +0200

    po zajeciach

---
+++ Lab3/Lab3/Matrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3 {
+    internal class Matrix {
+        public int[,] matrixClass { get; set; }       //[r,c]
+
+        public Matrix(int n) {
+            var rand = new Random();
+            matrixClass = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrixClass[i, j] = rand.Next(1,10);
+                }
+            }
+        }
+
+        public static Matrix Zeros(Matrix matrix1) {
+            for (int i = 0; i < GetSize(matrix1); i++) {
+                for (int j = 0; j < GetSize(matrix1); j++) {
+                    matrix1.matrixClass[i, j] = 0;
+                }
+            }
+
+            return matrix1;
+        }
+
+        public static int GetSize(Matrix matrix) {
+            return matrix.matrixClass.GetLength(0);
+        }
+
+        public override string ToString() {
+            string s = "";
+
+            for (int i = 0;i < matrixClass.GetLength(0);i++) {
+                s += "|";
+                for (int j = 0; j< matrixClass.GetLength(1); j++) {
+                    s += matrixClass[i, j] + "\t";
+                }
+                s += "|\n";
+            }
+
+            return s;
+        }
+
+        public static Matrix Multiply(Matrix matrix1, Matrix matrix2) {
+            Matrix result = new Matrix(GetSize(matrix1));
+            result = Zeros(result);
+
+            for (int i = 0; i<GetSize(matrix1); i++) {
+                for(int j=0; j<GetSize(matrix1);j++) {
+                    for(int k=0; k<GetSize(matrix1);k++) {
+                        result.matrixClass[i, j] += matrix1.matrixClass[i, k] * matrix2.matrixClass[k, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab3 {
+    internal class Program {
+        static void Main(string[] args) {
+            Matrix matrix1 = new Matrix(4);
+            Matrix matrix2 = new Matrix(4);
+            Matrix result = Matrix.Multiply(matrix1, matrix2);
+
+
+            Console.WriteLine(matrix1.ToString());
+            Console.WriteLine(matrix2.ToString());
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
